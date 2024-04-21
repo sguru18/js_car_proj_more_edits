81441a2b461e6657a5c36e76354ebddfb8364671
sguru18 <shguru110@gmail.com>
2024-04-21 11:32:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -75,18 +75,24 @@ const cars = generateCars(N);
 let bestCar = cars[0];
 if(localStorage.getItem("bestBrain")){
     for(let i = 0; i<cars.length;i++){
-        cars[i].brain = JSON.parse(
-            localStorage.getItem("bestBrain"));
+        // cars[i].brain = JSON.parse(
+        //     localStorage.getItem("bestBrain"));
     //     fetch('https://car-project-xi.vercel.app/getBrainData')
     //     .then(response => response.json())
     //     .then(data => {
     //     cars[i].brain=data
     // })
     // .catch(error => console.error('Error loading brain data:', error));
+    fetch('bestBrain.json')
+        .then(response => response.json()) // Parse the JSON response
+        .then(data => {
+            cars[i].brain = data; // Assign the parsed data to the brain property
+        })
+        .catch(error => console.error('Error loading brain data:', error));
+    }
 
-        if(i!=0){
+    if(i!=0){
             NeuralNetwork.mutate(cars[i].brain, 0.15);
-        }
     }
 }
 
